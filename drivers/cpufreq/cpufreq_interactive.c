58646fcbc56d607e6c289446402c34e17d0bae11
Batu33-TR <batuhanarslanoglu4@gmail.com>
2020-12-15 18:05:23 +0000

    interactive: Set Screen Off Max Freq to 1248MHz
    Signed-off-by: Batu33-TR <batuhanarslanoglu4@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -170,7 +170,7 @@ struct cpufreq_interactive_tunables {
 	bool powersave_bias;
 
 	/* Maximum frequency while the screen is off */
-#define DEFAULT_SCREEN_OFF_MAX 1401600
+#define DEFAULT_SCREEN_OFF_MAX 1248000
 	unsigned long screen_off_max;
 };
 
